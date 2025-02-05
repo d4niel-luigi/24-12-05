2ff78e0154d209a54c1e64c462a02d315bf009ba
Petrik Diak <diak@petrik.hu>
2025-02-05 14:03:05 +0100

    kesz a gyerek controller

--- src/gyerek/dto/create-gyerek.dto.ts
+++ src/gyerek/dto/create-gyerek.dto.ts
@@ -1,16 +1,27 @@
-import { IsBoolean, IsNotEmpty, IsString } from "class-validator";
+import { IsBoolean, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { ApiProperty  } from "@nestjs/swagger";
 
 export class CreateGyerekDto {
   @IsString()
+  @MinLength(3)
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'Piroska'
+  })
   nev: string
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'Magyarország, Budapest, Farkas utca 14.'
+  })
   cim: string
 
   @IsBoolean()
   @IsNotEmpty()
+  @ApiProperty({
+    example: true
+  })
   jovolte: boolean
 }
 
--- src/gyerek/gyerek.controller.ts
+++ src/gyerek/gyerek.controller.ts
@@ -2,60 +2,98 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Put, NotFoundExcepti
 import { GyerekService } from './gyerek.service';
 import { CreateGyerekDto } from './dto/create-Gyerek.dto';
 import { UpdateGyerekDto } from './dto/update-Gyerek.dto';
+import { ApiResponse, ApiParam, ApiBadRequestResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { Gyerek } from './entities/gyerek.entity';
 
-@Controller('gyerek')
+@Controller('Gyerek')
+@ApiBearerAuth()
 export class GyerekController {
   constructor(private readonly GyerekService: GyerekService) {}
 
-  @Post()
-  create(@Body() CreateGyerekDto: CreateGyerekDto) {
-    return this.GyerekService.create(CreateGyerekDto);
+   /**
+   * Creates a new child entry in the catalog
+   * 
+   * @param id The unique ID of the child
+   * @param createGyerekDto The data to be created
+   * @returns JSON response 
+   */
+   @Patch(':id')
+   @ApiParam({
+     name: "id",
+     type: "number",
+     description: 'The unique ID of the child'
+   })
+   @ApiResponse({ status: 200, description: 'child created successfully' })
+   @ApiBadRequestResponse({ description: 'The supplied data was invalid' })
+   @Post()
+   create(@Body() createGyerekDto: CreateGyerekDto) {
+    return this.GyerekService.create(createGyerekDto);
   }
 
+   /**
+   * Retrieves all child entries in the catalog
+   * 
+   * @returns JSON response
+   */
   @Get()
+  @ApiResponse({ status: 200, description: 'Retrice all childs' })
+  @ApiBadRequestResponse({ description: 'The supplied data was invalid' })
   findAll() {
     return this.GyerekService.findAll();
   }
 
+  /**
+   * Retrieves a specific child by ID
+   * 
+   * @param id The unique ID of the child
+   * @returns JSON response
+   */
   @Get(':id')
-  async findOne(@Param('id') id: string) {
-    const gyerek = await this.GyerekService.findOne(+id);
-    if(!gyerek){
-      throw new NotFoundException('Nincs ilyen id-val rendelkező gyerek!')
-    }
-    return gyerek;
-  }
-  
-  @Put(':gyerekId/jatek/:jatekId')
-  async addToyToChild(@Param('gyerekId') gyerekId: string, @Param('jatekId') jatekId: string) {
-    const result = await this.GyerekService.addToyToChild(+gyerekId, +jatekId);
-    if (!result) {
-      throw new NotFoundException('Nincs ilyen ID-val rendelkező gyerek vagy játék!');
-    }
-    return result;
-  }
-  @Delete(':gyerekId/jatek/:jatekId')
-  async removeToyFromChild(@Param('gyerekId') gyerekId: string, @Param('jatekId') jatekId: string) {
-    const result = await this.GyerekService.removeToyFromChild(+gyerekId, +jatekId);
-    if (!result) {
-      throw new NotFoundException('Nincs ilyen ID-val rendelkező gyerek vagy játék!');
-    }
-    return result;
+  @ApiParam({
+    name: "id",
+    type: "number",
+    description: 'The unique ID of the child'
+  })
+  @ApiResponse({ status: 200, description: 'Retrice all childs' })
+  @ApiBadRequestResponse({ description: 'child not found or invalid ID' })
+  findOne(@Param('id') id: string) {
+    return this.GyerekService.findOne(+id);
   }
 
-  @Delete(':id')
-  async  remove(@Param('id') id: string) {
-    if (!await this.GyerekService.remove(+id)){
-      throw new  NotFoundException('Nincs ilyen id-val rendelkező gyerek!')
-    }
-  }
-
+  /**
+   * Modifies the details of an existing child
+   * 
+   * @param id The unique ID of the child
+   * @param updatechildtalogDto The data to modify
+   * @returns JSON response 
+   */
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() UpdateGyerekDto: UpdateGyerekDto) {
-    const gyerek = await this.GyerekService.update(+id, UpdateGyerekDto);
-    if(!gyerek){
-      throw new NotFoundException('Nincs ilyen id-val rendelkező játék!')
-    }
-    return gyerek;
+  @ApiParam({
+    name: "id",
+    type: "number",
+    description: 'The unique ID of the child'
+  })
+  @ApiResponse({ status: 200, description: 'The modified data of the phone' })
+  @ApiBadRequestResponse({ description: 'The supplied data was invalid' })
+  update(@Param('id') id: string, @Body() updateGyerekDto: UpdateGyerekDto) {
+    return this.GyerekService.update(+id, updateGyerekDto);
+  }
+
+   /**
+   * Deletes a child entry by ID
+   * 
+   * @param id The unique ID of the child
+   * @returns JSON response
+   */
+   @Delete(':id')
+   @ApiParam({ 
+    name: 'id', 
+    type: 'number', 
+    description: 'The unique ID of the child' })
+  @ApiParam({ name: 'id', type: 'int', description: 'The unique ID of the child' })
+  @ApiResponse({ status: 200, description: 'child deleted successfully' })
+  @ApiBadRequestResponse({ description: 'child not found or invalid ID' })
+  remove(@Param('id') id: string) {
+    return this.GyerekService.remove(+id);
   }
 }
